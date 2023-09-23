c26c316b9e568c81fbbca16884fa5557678bd86b
hmsmiraz <hmsmiraz64729@gmail.com>
2023-09-23 15:40:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-7xl text-center font-bold text-purple-600'>Career Hub</h1>
+    </>
+  )
+}
+
+export default App
